56aef8045fb3ffbff65c38a94e8d6dcde65551f3
isaac <isaacmojica09@hotmail.com>
2025-06-24 17:33:16 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/api/saludo', methods=['GET'])
+def saludo():
+    # En un caso real, aquí conectarías con MySQL y obtendrías datos
+    return jsonify({"mensaje": "¡Hola desde mi API en Docker!", "version": "1.0"})
+
+if __name__ == '__main__':
+    # Escucha en todas las interfaces para Docker
+    app.run(host='0.0.0.0', port=5000)
