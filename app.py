b87568ce7390db69f18c3832c7893e96d3e63c3e
Justin <94204946+JustinoLM@users.noreply.github.com>
2025-06-30 19:56:00 -0500

    Update app.py v31

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/api/saludo', methods=['GET'])
 def saludo():
     # En un caso real, aquí conectarías con MySQL y obtendrías datos
-    return jsonify({"mensaje": "¡Hola desde mi API ACTUALIZADA en Docker!", "version": "3.0"})
+    return jsonify({"mensaje": "¡Hola desde mi API ACTUALIZADA en Docker!", "version": "4.0"})
 
 if __name__ == '__main__':
     # Escucha en todas las interfaces para Docker
